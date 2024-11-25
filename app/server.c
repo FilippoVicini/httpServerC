9c58f736f7e48416c1a6d4ccbc9020e3d9535403
FilippoVicini <filippo.vicini2@gmail.com>
2024-11-25 13:12:27 +0000

    multiple request bug fix

--- app/server.c
+++ app/server.c
@@ -1,13 +1,13 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
 bool starts_With(const char *pre, const char *str) {
   size_t lenpre = strlen(pre), lenstr = strlen(str);
   return lenstr < lenpre ? false : memcmp(pre, str, lenpre) == 0;
@@ -30,121 +30,134 @@ char *extract_between(const char *str, const char *p1, const char *p2) {
     }
   }
 }
-int main() {
-  // Disable output buffering
-  setbuf(stdout, NULL);
-  setbuf(stderr, NULL);
 
-  // You can use print statements as follows for debugging, they'll be visible
-  printf("Logs from your program will appear here!\n");
+void *handle_client(void *arg) {
+  int fd = *(int *)arg;
+  free(arg); // Free the dynamically allocated fd pointer
 
-  //
-  int server_fd, client_addr_len;
-  struct sockaddr_in client_addr;
-
-  server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd == -1) {
-    printf("Socket creation failed: %s...\n", strerror(errno));
-    return 1;
+  char buffer[4096];
+  int n = read(fd, buffer, sizeof(buffer) - 1);
+  if (n <= 0) {
+    close(fd);
+    return NULL;
   }
+  buffer[n] = '\0';
+  printf("Received request:\n%s\n", buffer);
 
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
-      0) {
-    printf("SO_REUSEADDR failed: %s \n", strerror(errno));
-    return 1;
-  }
-
-  struct sockaddr_in serv_addr = {
-      .sin_family = AF_INET,
-      .sin_port = htons(4221),
-      .sin_addr = {htonl(INADDR_ANY)},
-  };
-
-  if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
-    printf("Bind failed: %s \n", strerror(errno));
-    return 1;
-  }
-
-  int connection_backlog = 5;
-  if (listen(server_fd, connection_backlog) != 0) {
-    printf("Listen failed: %s \n", strerror(errno));
-    return 1;
-  }
-  while (1) {
-    printf("Waiting for a client to connect...\n");
-    client_addr_len = sizeof(client_addr);
-
-    char *response = "HTTP/1.1 200 OK\r\n\r\n";
-    int fd =
-        accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    printf("Client connected\n");
-    send(fd, response, strlen(response), 0);
-
-    char buffer[4096];
-    int n = read(fd, buffer, sizeof(buffer));
-
-    printf("messgae %s\n", buffer);
-
-    if (starts_With("GET /echo/", buffer)) {
-      // Handle `/echo` request
-      // "GET /echo/abababab HTTP/1.1"
-
-      char *text = extract_between(buffer, "GET /echo/", " HTTP/1.1");
-      char *reply;
-      printf("output %s\n", text);
-      printf("size %lu\n", strlen(text));
-
+  if (starts_With("GET /echo/", buffer)) {
+    // Handle `/echo` request
+    char *text = extract_between(buffer, "GET /echo/", " HTTP/1.1");
+    if (text) {
       char *msg;
       asprintf(&msg,
-               "HTTP/1.1 200 OK\r\n"
-               "Content-Type: text/plain\r\n\r\n",
-               "Echo: %s", text);
-
+               "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nEcho: %s",
+               text);
       send(fd, msg, strlen(msg), 0);
-      printf("test output %s", text);
-
-    } else if (starts_With("GET / HTTP/1.1", buffer)) {
-      // Handle `/` request
-      char *reply =
-          "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nWelcome "
-          "to the root path!";
+      free(msg);
+      free(text);
+    } else {
+      char *reply = "HTTP/1.1 400 Bad Request\r\n\r\nInvalid Echo Request";
       send(fd, reply, strlen(reply), 0);
-      printf("Handled / request\n");
-
-    } else if (starts_With("GET /user-agent HTTP/1.1", buffer)) {
-
-      char *text = extract_between(buffer, "User-Agent: ", "\r\n");
-      char *reply;
+    }
+  } else if (starts_With("GET / HTTP/1.1", buffer)) {
+    char *reply = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nWelcome "
+                  "to the root path!";
+    send(fd, reply, strlen(reply), 0);
+  } else if (starts_With("GET /user-agent HTTP/1.1", buffer)) {
+    char *text = extract_between(buffer, "User-Agent: ", "\r\n");
+    if (text) {
       char *msg;
       asprintf(&msg,
-               "HTTP/1.1 200 OK\r\n"
-               "Content-Type: text/plain\r\n"
-               "Content-Length: %lu\r\n\r\n"
-               "Echo: %s",
-               strlen(text) + 6, text);
+               "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "
+               "%lu\r\n\r\n%s",
+               strlen(text), text);
       send(fd, msg, strlen(msg), 0);
-      printf("output %s\n", text);
-      printf("size %lu\n", strlen(text));
+      free(msg);
+      free(text);
+    } else if (starts_With("GET /files/", buffer)) {
+      char *text = extract_between(buffer, "User-Agent: ", "\r\n");
+      printf("filename%s\n", text);
 
     }
 
     else {
+      char *reply = "HTTP/1.1 400 Bad Request\r\n\r\nMissing User-Agent";
+      send(fd, reply, strlen(reply), 0);
+    }
+  } else {
+    char *reply = "HTTP/1.1 404 Not Found\r\n\r\n";
+    send(fd, reply, strlen(reply), 0);
+  }
 
-      char *reply = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-      int bytes_send = send(fd, reply, strlen(reply), 0);
+  close(fd);
+  return NULL;
+}
+int main() {
+  setbuf(stdout, NULL);
+
+  int server_fd;
+  struct sockaddr_in serv_addr;
+
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (server_fd == -1) {
+    perror("Socket creation failed");
+    return 1;
+  }
+
+  int reuse = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+      0) {
+    perror("SO_REUSEADDR failed");
+    close(server_fd);
+    return 1;
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(4221);
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if (bind(server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) != 0) {
+    perror("Bind failed");
+    close(server_fd);
+    return 1;
+  }
+
+  if (listen(server_fd, 5) != 0) {
+    perror("Listen failed");
+    close(server_fd);
+    return 1;
+  }
+
+  printf("Server is listening on port 4221...\n");
+
+  while (1) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int client_fd =
+        accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    if (client_fd < 0) {
+      perror("Accept failed");
+      continue;
     }
 
-    char *data = strstr(buffer, "\r\n\r\n");
-    if (data != NULL) {
-      data += 4;
-      // printf("data", data);
+    int *pclient = malloc(sizeof(int));
+    if (!pclient) {
+      perror("Malloc failed");
+      close(client_fd);
+      continue;
     }
+    *pclient = client_fd;
+
+    pthread_t thread_id;
+    if (pthread_create(&thread_id, NULL, handle_client, pclient) != 0) {
+      perror("Thread creation failed");
+      close(client_fd);
+      free(pclient);
+      continue;
+    }
+    pthread_detach(thread_id);
   }
 
   close(server_fd);
-
   return 0;
 }
